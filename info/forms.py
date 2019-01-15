ce209c4437357433359cbcc30f7741c966f1e664
cliffnyendwe"git config --global user.name "cliffnyendwe"git commit -m"add fix indentation error"git status <cliffnyendwe>
2019-01-15 20:15:54 +0300

    made the main initializing

---
+++ info/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Profile,NeighborHood,Post,Comment
+from django.contrib.auth.models import User
+
+
+class ProfileForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    exclude = ['user']
+
+
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighborHood
+        exclude= ['occupants']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user','neighborHood','pub_date']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name']
